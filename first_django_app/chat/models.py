4b8e1bd6738de8e4c8f160f0f7e073a2a448b3e3
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2024-02-01 17:07:43 +0100

    progress

--- first_django_app/chat/models.py
+++ first_django_app/chat/models.py
@@ -1,3 +1,14 @@
+from datetime import date
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class Message(models.Model):
+    text = models.CharField(max_length=500)
+    #chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='chat_message_set', default=None, blank=True, null=True)
+    created_at = models.DateField(default=date.today)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')
+    receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver_message_set')
+#chat = Chat Klasse verknüpfen
+#Direkt auswählen aus den registrieten User im Admin Infterface
+#on_delete -> Sollte ein User gelöscht werden, so werden alle Nachrichten entfernt
+#related_name -> Information für die Datenbank, damit diese weiß worum es geht.
