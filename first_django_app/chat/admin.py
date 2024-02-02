7f133c9ee66b33cd3011a4081fd79e8aa4b235f9
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2024-02-02 16:03:30 +0100

    progress

--- first_django_app/chat/admin.py
+++ first_django_app/chat/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Message, Chat
 
-# Register your models here.
+
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('chat','text', 'created_at', 'author', 'receiver')
+    list_display = ('created_at', 'author', 'text', 'receiver')
+    search_fields = ('author',)
+
+
+admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
--- first_django_app/chat/models.py
+++ first_django_app/chat/models.py
@@ -2,13 +2,17 @@ from datetime import date
 from django.conf import settings
 from django.db import models
 
+class Chat(models.Model):
+    created_at = models.DateField(default=date.today)
+
 class Message(models.Model):
     text = models.CharField(max_length=500)
-    #chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='chat_message_set', default=None, blank=True, null=True)
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='chat_message_set', default=None, blank=True, null=True)
     created_at = models.DateField(default=date.today)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')
     receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver_message_set')
-#chat = Chat Klasse verknüpfen
-#Direkt auswählen aus den registrieten User im Admin Infterface
+#chat = blank=Null der Standartwert ist nichts, blank=True es darf nichts hineingegeben werden, null=True Datenbank akzeptiert auf nichts
+#Wird benötigt falls im Laufe das Model erweitert wird und ältere Nachrichten ohne dem chat keinen Fehler aufweisen.
+#author -> Direkt auswählen aus den registrieten User im Admin Infterface
 #on_delete -> Sollte ein User gelöscht werden, so werden alle Nachrichten entfernt
 #related_name -> Information für die Datenbank, damit diese weiß worum es geht.
