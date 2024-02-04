baeab44ddfae62fbdcf5d23563e1cd7eb4e6bd37
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2024-02-04 14:41:37 +0100

    progress

--- first_django_app/chat/views.py
+++ first_django_app/chat/views.py
@@ -4,20 +4,21 @@ from .models import Message,Chat
 from django.contrib.auth import authenticate, login, get_user_model, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
+from django.contrib.auth.decorators import login_required
 
 @login_required(login_url='/login/')
 
 def index(request):
- if request.method == 'POST':
-     print('received data' + request.POST['textmessage'])
-     myChat = Chat.objects.get(id=1)
-     Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
- chatMessages = Message.objects.filter(chat__id=1)
- return render(request, 'chat/index.html',  {"messages" : chatMessages})
+    if request.method == 'POST':
+        print('received data' + request.POST['textmessage'])
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+    chatMessages = Message.objects.filter(chat__id=1)
+    return render(request, 'chat/index.html', {"messages": chatMessages})
 
 def logout_view(request):
   logout(request)
-  return render(request, 'login/login.html')
+  return render(request, '/login/')
 
 
 def login_view(request):
