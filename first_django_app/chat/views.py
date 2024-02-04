02f1f1fcfb342045cfd43dc7dd8de9414d581659
WalterDoni <133245257+WalterDoni@users.noreply.github.com>
2024-02-04 18:01:20 +0100

    progres

--- first_django_app/chat/views.py
+++ first_django_app/chat/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth import authenticate, login, get_user_model, logout
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
+from django.core import serializers # =Ein Objekt in die richtige Form bringen
 
 @login_required(login_url='/login/')
 
@@ -12,7 +14,12 @@ def index(request):
     if request.method == 'POST':
         print('received data' + request.POST['textmessage'])
         myChat = Chat.objects.get(id=1)
-        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+        new_message = Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
+        serialized_obj = serializers.serialize('json', [ new_message ])
+        return JsonResponse(serialized_obj[1:-1], safe=False)
+    # Achtung [1:-1] weil sich das ganze in einem Array befindet.
+    #Jedoch wird ein JSON benötigt. Somit werden die Zeichen[ und ]
+    #entfert. Somit ist es ein JSON.
     chatMessages = Message.objects.filter(chat__id=1)
     return render(request, 'chat/index.html', {"messages": chatMessages})
 
